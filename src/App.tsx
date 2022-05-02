670a8f74262d43c00dba7ad49e73b807681e0e8c
飲むCorona <danielcorona313@gmail.com>
2022-05-02 15:40:34 -0300

    ep-1

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { Widget } from "./components/Widget";
+
+export function App(){
+  return <Widget/>
+
+
+}
+
+
+
+
+
+// interface HellowsProps {
+//   text?: string;
+// }
+
+// function Hellows(props: HellowsProps){
+//   return <button className="bg-violet-500 px-4 h-10 rounded text-violet-100 hover:bg-violet-700 transi">{props.text ?? 'Seu nome'}</button>
+// }
+
+// function App() {
+//   return (
+//     <div className='flex gap-2'>
+//       <Hellows text="Daniel" />
+//       <Hellows text="Thiago" />
+//       <Hellows />
+//       <Hellows text="Victor" />
+//     </div>
+//   ) 
+// }
+// //Componente: Função que retorna HTML
+// //Componentes começam com letra maiúscula por padrão (arquivo e nome da função)
+// export default App
